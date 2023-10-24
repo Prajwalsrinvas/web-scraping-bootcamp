5cf92f3328d61c8ca40043a711a6e9b4389e29d1
Prajwalsrinvas <prajwalsrinvas@gmail.com>
2023-10-24 19:45:15 +0530

    post-process data for cleaning gdp and year values

--- week8/countries_gdp/countries_gdp/items.py
+++ week8/countries_gdp/countries_gdp/items.py
@@ -3,9 +3,29 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+import re
+
 import scrapy
 from itemloaders.processors import MapCompose, TakeFirst
 from w3lib.html import remove_tags
+
+
+def remove_commas(value):
+    return value.replace(",", "")
+
+
+def convert_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+
+def extract_year(value):
+    year = re.findall(r"\d{4}", value)
+    if not year:
+        return value
+    return year
 
 
 class CountryGdpItem(scrapy.Item):
@@ -18,8 +38,10 @@ class CountryGdpItem(scrapy.Item):
         input_processor=MapCompose(remove_tags, str.strip), output_processor=TakeFirst()
     )
     gdp = scrapy.Field(
-        input_processor=MapCompose(remove_tags, str.strip), output_processor=TakeFirst()
+        input_processor=MapCompose(remove_tags, str.strip, remove_commas, convert_int),
+        output_processor=TakeFirst(),
     )
     year = scrapy.Field(
-        input_processor=MapCompose(remove_tags, str.strip), output_processor=TakeFirst()
+        input_processor=MapCompose(remove_tags, str.strip, extract_year, convert_int),
+        output_processor=TakeFirst(),
     )
