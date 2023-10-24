85950c1f676767739a870b4e7d539f4f6d609420
Prajwalsrinvas <prajwalsrinvas@gmail.com>
2023-10-24 20:17:42 +0530

    save data to sqlite db

--- week8/countries_gdp/countries_gdp/pipelines.py
+++ week8/countries_gdp/countries_gdp/pipelines.py
@@ -5,6 +5,8 @@
 
 
 # useful for handling different item types with a single interface
+import sqlite3
+
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 
@@ -14,3 +16,33 @@ class CountriesGdpPipeline:
         if not isinstance(item["gdp"], int):
             raise DropItem("Item excluded as gdp is missing")
         return item
+
+
+class SaveToDatabasePipeline:
+    def __init__(self):
+        self.con = sqlite3.connect("countries_gdp.db")
+        self.cur = self.con.cursor()
+
+    def open_spider(self, spider):
+        self.cur.execute(
+            """
+        CREATE TABLE IF NOT EXISTS countries_gdp 
+        (country_name TEXT PRIMARY KEY,
+        region TEXT,
+        gdp INTEGER,
+        year INTEGER) 
+        """
+        )
+        self.con.commit()
+
+    def process_item(self, item, spider):
+        self.con.execute(
+            """
+        INSERT INTO countries_gdp (country_name, region, gdp, year) VALUES (?, ?, ?, ?)        
+        """,
+            (item["country_name"], item["region"], item["gdp"], item["year"]),
+        )
+        self.con.commit()
+
+    def close_spider(self, spider):
+        self.con.close()
--- week8/countries_gdp/countries_gdp/settings.py
+++ week8/countries_gdp/countries_gdp/settings.py
@@ -64,6 +64,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     "countries_gdp.pipelines.CountriesGdpPipeline": 300,
+    "countries_gdp.pipelines.SaveToDatabasePipeline": 400,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
